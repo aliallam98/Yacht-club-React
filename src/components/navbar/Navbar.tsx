618a25889d0efb2d6260c2bcb4ac4a0291c214cd
aliallam98 <allam98.ali@gmail.com>
2024-05-04 13:55:43 +0300

    Finished ...

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,11 +2,19 @@ import { Link, useLocation } from "react-router-dom";
 import { navLinks } from "../../constants";
 import Logo from "../Logo";
 import useNavbarScroll from "../../hooks/useNavBar";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenuShow, setIsMenuShow] = useState(true);
   const { pathname } = useLocation();
   const isScroll = useNavbarScroll();
 
+  const menuCloseHandler = () => {
+    setIsMenuShow(false);
+  };
+  const menuOpenHandler = () => {
+    setIsMenuShow(true);
+  };
   return (
     <header
       className={`${"p-4 fixed top-0 w-full z-50 text-black"} ${
@@ -15,13 +23,13 @@ const Navbar = () => {
     >
       <nav className="container flex items-center justify-between">
         <Logo />
-        <ul className="flex items-center gap-2">
+        <ul className="hidden lg:flex items-center gap-2  ">
           {navLinks.map((link, i) => {
             return (
               <Link
                 key={i}
                 className={`${
-                  pathname === link.path && "text-blue-600"
+                  pathname === link.path && "text-sky-500"
                 }  capitalize tracking-wide`}
                 to={link.path}
               >
@@ -30,7 +38,40 @@ const Navbar = () => {
             );
           })}
         </ul>
+        <button onClick={menuOpenHandler} className="block lg:hidden ">
+          <img
+            src="/1024px-Hamburger_icon_white.svg.png"
+            width={30}
+            height={30}
+            alt="burger-menu"
+          />
+        </button>
       </nav>
+      {/* Mobile Menu */}
+      {isMenuShow && (
+        <ul className="absolute lg:hidden left-1/2 top-0 -translate-x-1/2 flex flex-col gap-y-4 py-16  items-center bg-white  w-full h-screen">
+          {navLinks.map((link, i) => {
+            return (
+              <Link
+                key={i}
+                className={`${
+                  pathname === link.path && "text-blue-600"
+                }  capitalize tracking-wide`}
+                to={link.path}
+                onClick={menuCloseHandler}
+              >
+                {link.label}
+              </Link>
+            );
+          })}
+          <span
+            className="absolute top-6 right-6 font-bold text-xl"
+            onClick={menuCloseHandler}
+          >
+            X
+          </span>
+        </ul>
+      )}
     </header>
   );
 };
--- src/components/rallies/EventItem.tsx
+++ src/components/rallies/EventItem.tsx
@@ -1,39 +1,32 @@
 interface IProps {
-    image: string;
-    date: string;
-    title: string;
-    description: string;
-    link: string;
-  }
-  
-  const EventItem = ({
-    image,
-    title,
-    description,
-    date,
-    link,
-  }: IProps) => {
-    return (
-      <article className=" relative bg-white max-w-[380px] pb-10">
-        <img
-          src={image}
-          alt="Destinations image"
-          className="max-w-full object-cover"
-        />
-        <div className="p-2 px-4 space-y-1">
-          <p className="text-neutral-400 text-sm">{date}</p>
-          <p className="text-xl">{title}</p>
-          <p className="text-neutral-400 text-sm max-w-sm ">
-            {description.split(" ").slice(0, 10).join(" ")}
-          </p>
-  
-          <button className="absolute bottom-2 left-0 capitalize py-2 px-4 text-sm -tracking-wide">
-            {link}
-          </button>
-        </div>
-      </article>
-    );
-  };
-  
-  export default EventItem;
-  
+  image: string;
+  date: string;
+  title: string;
+  description: string;
+  link: string;
+}
+
+const EventItem = ({ image, title, description, date, link }: IProps) => {
+  return (
+    <article className=" relative bg-white max-w-[380px] pb-10 mx-auto">
+      <img
+        src={image}
+        alt="Destinations image"
+        className="max-w-full object-cover"
+      />
+      <div className="p-2 px-4 space-y-1">
+        <p className="text-neutral-400 text-sm">{date}</p>
+        <p className="text-xl">{title}</p>
+        <p className="text-neutral-400 text-sm max-w-sm ">
+          {description.split(" ").slice(0, 10).join(" ")}
+        </p>
+
+        <button className="absolute bottom-2 left-0 capitalize py-2 px-4 text-sm -tracking-wide">
+          {link}
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default EventItem;
